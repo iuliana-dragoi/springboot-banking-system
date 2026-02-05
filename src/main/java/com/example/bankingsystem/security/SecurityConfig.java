50fa7657f5b4dbccc0ec8495fb6db003938b3dfa
iuliana.dragoi <iulianadragoi95@gmail.com>
2026-02-05 13:36:54 +0200

    Implement AOP for metrics

--- src/main/java/com/example/bankingsystem/security/SecurityConfig.java
+++ src/main/java/com/example/bankingsystem/security/SecurityConfig.java
@@ -1,6 +1,6 @@
 package com.example.bankingsystem.security;
 
-import com.example.bankingsystem.authentication.DynamicAuthenticationFilter;
+import com.example.bankingsystem.security.authentication.DynamicAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
